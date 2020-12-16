427d0db63c59b963f74e44a3fda1f22e948f8cbc
RinaOcean <antyfifa@gmail.com>
2020-12-16 23:42:48 +0200

    переделала 2 задание с bind

--- js/task-2.js
+++ js/task-2.js
@@ -5,12 +5,12 @@ const inventory = {
   add(itemName) {
     console.log(`Adding ${itemName} to inventory`);
 
-    inventory.items.push(itemName);
+    this.items.push(itemName);
   },
   remove(itemName) {
     console.log(`Removing ${itemName} from inventory`);
 
-    inventory.items = inventory.items.filter((item) => item !== itemName);
+    this.items = this.items.filter((item) => item !== itemName);
   },
 };
 
@@ -19,13 +19,13 @@ const invokeInventoryAction = function (itemName, action) {
   action(itemName);
 };
 
-invokeInventoryAction("Medkit", inventory.add);
+invokeInventoryAction("Medkit", inventory.add.bind(inventory));
 // Invoking action on Medkit
 // Adding Medkit to inventory
 
 console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
 
-invokeInventoryAction("Gas mask", inventory.remove);
+invokeInventoryAction("Gas mask", inventory.remove.bind(inventory));
 // Invoking action on Gas mask
 // Removing Gas mask from inventory
 
